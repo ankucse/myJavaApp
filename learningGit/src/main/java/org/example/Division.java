d7a806f2f601f07d6f1c8f5d65754a8fcd3058ca
Ankit Sinha <ankitsinha.vkit@gmail.com>
2024-10-13 13:48:19 +0530

    Commit message describing the changes

--- learningGit/src/main/java/org/example/Division.java
+++ learningGit/src/main/java/org/example/Division.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Division {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values of a and b");
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println("The division of two numbers is : " + (a/b));
+    }
 }
